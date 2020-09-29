6d69375c642292a1b409944cb7aa68c601e6aaed
pbeckinger <31124349+pbeckinger@users.noreply.github.com>
2020-09-29 09:49:08 -0500

    Update baseurl for axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,9 @@ export default {
     '~/api/index.js',
   ],
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: {
+    baseURL:"/"
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
